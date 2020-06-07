71d91fb9b9bdc34302f6680e526791b3ca19e8bd
thiagovilarinholemes <lemes_vilarinho@yahoo.com.br>
2020-06-07 20:18:01 -0300

    OrderStatus enum

--- src/main/java/gd/rf/thiagolemes/coursejava/entities/Order.java
+++ src/main/java/gd/rf/thiagolemes/coursejava/entities/Order.java
@@ -1,6 +1,7 @@
 package gd.rf.thiagolemes.coursejava.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import gd.rf.thiagolemes.coursejava.entities.enums.OrderStatus;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -14,8 +15,11 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT ")
     private Instant moment;
+
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -23,10 +27,11 @@ public class Order implements Serializable {
 
     public Order(){}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -51,6 +56,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
