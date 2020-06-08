79b09eb951aee0778e96d392c486aa1eecfd7214
thiagovilarinholemes <lemes_vilarinho@yahoo.com.br>
2020-06-08 14:58:32 -0300

    Exception handling - delete

---
+++ src/main/java/gd/rf/thiagolemes/coursejava/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package gd.rf.thiagolemes.coursejava.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
