210a52caa085d3b0ece0d5e43cb5158de6162ca2
thiagovilarinholemes <lemes_vilarinho@yahoo.com.br>
2020-06-08 14:41:29 -0300

    Exception handling - findById

--- src/main/java/gd/rf/thiagolemes/coursejava/services/UserService.java
+++ src/main/java/gd/rf/thiagolemes/coursejava/services/UserService.java
@@ -2,6 +2,7 @@ package gd.rf.thiagolemes.coursejava.services;
 
 import gd.rf.thiagolemes.coursejava.entities.User;
 import gd.rf.thiagolemes.coursejava.repositories.UserRepository;
+import gd.rf.thiagolemes.coursejava.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     // Retorna o usuário salvo
---
+++ src/main/java/gd/rf/thiagolemes/coursejava/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package gd.rf.thiagolemes.coursejava.services.exceptions;
+
+public class ResourceNotFoundException extends  RuntimeException{
+
+    public ResourceNotFoundException(Object id){
+        super("Resource not found. Id " + id);
+    }
+}
